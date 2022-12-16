54d5b23272ca3159cfc2686bb27db1d8169cddee
Fajrin Azzimi <fajrinazzimi@gmail.com>
2022-12-16 15:00:56 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Fajrin Azzimi" // TODO: replace this
+const studentId = "FE3071115" // TODO: replace this
+
+export { url, studentName, studentId }
